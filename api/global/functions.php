b15613583a5c867a03ed3008479ce75c17e0eaa5
Pablo Abreu <pabreu.ar@gmmail.com>
2020-06-26 02:21:24 -0300

    prueba QR

--- api/global/functions.php
+++ api/global/functions.php
@@ -10,7 +10,7 @@ $country_id="MLA";
 // No tocar el integrator_id para el ejercicio
 $integrator_id_test="XXXXXXX"; 
 
-$notificationJSON="";
+$notificationJSON="https://eiffelsoft-mpqr.herokuapp.com/api/notifications";
 global $access_token,$collector_id,$notificationJSON,$country_id;
 
 
